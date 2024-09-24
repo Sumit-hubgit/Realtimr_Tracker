c12ebe62f0a5207522b121b2274735cbd41bce12
Sumit-hubgit <sumitsahu7017@gmail.com>
2024-09-24 15:55:59 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+const socket = io();    //connection request will besent to backend
+console.log("hey")
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position)=>{
+        const {latitude , longitude}=position.coords;
+        socket.emit("send-location",{latitude , longitude});
+    },
+    (error)=>{
+        console.log(error);
+    },{
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0
+    });
+}
+const map = L.map("map").setView([0,0],10);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"Sumit Maps"
+}).addTo(map)
+
+const vehicleIcon = L.icon({
+    iconUrl: 'https://img.icon8.com/doodle/48/car--v1.png',
+    iconSize:[48,48],
+    iconAnchor:[25,25],
+    popupAnchor:[0,-25]
+});
+const vehicleMarker = L.marker([0,0],{icon:vehicleIcon}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location",(data)=>{
+    const{id, latitude,longitude}=data;
+    map.setView([latitude,longitude],16);
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude]);
+    }
+    else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
